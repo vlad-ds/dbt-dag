135c68ed803d0f381709b3f939f959e46cb20f01
Vlad Gheorghe <contact@vlad-gheorghe.com>
2023-02-18 10:57:10 +0100

    Populate graph

--- src/DbtDag.py
+++ src/DbtDag.py
@@ -9,16 +9,19 @@ class DbtNode:
                  node_dict: dict):
 
         self.full_name = full_name
-        with full_name.split(".") as parts:
-            self.node_type = parts[0]
-            self.dbt_project = parts[1]
-            self.node_name = parts[2]
-            self.run_id = parts[3] if len(parts) > 3 else None
+        parts = full_name.split(".")
+        self.node_type = parts[0]
+        self.dbt_project = parts[1]
+        self.node_name = parts[2]
+        self.run_id = parts[3] if len(parts) > 3 else None
 
         self.node_dict = node_dict
 
     def __hash__(self):
         return hash(self.full_name)
+
+    def __str__(self):
+        return self.full_name
 
 
 class DbtDag:
@@ -41,5 +44,11 @@ class DbtDag:
     def dbt_schema_version(self):
         return self.manifest["metadata"]["dbt_schema_version"]
 
+    def populate(self):
+        nodes = [DbtNode(full_name, node_dict)
+                 for full_name, node_dict in self.manifest["nodes"].items()]
+        self.graph.add_nodes_from(nodes)
+
 
 ddag = DbtDag(manifest_path="examples/jaffle_shop/manifest.json")
+ddag.populate()
