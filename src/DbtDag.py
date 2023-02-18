1d9df0dd34bb847a23c95c74868233ae9e5d1a0f
Vlad Gheorghe <contact@vlad-gheorghe.com>
2023-02-18 10:26:07 +0100

    Add features

--- src/DbtDag.py
+++ src/DbtDag.py
@@ -5,10 +5,11 @@ import networkx as nx
 
 class DbtDag:
     def __init__(self,
-                manifest_path: str = None,
-                manifest_dict: dict = None):
+                 manifest_path: str = None,
+                 manifest_dict: dict = None):
 
         self.manifest = None
+        self.graph = nx.DiGraph()
 
         if manifest_dict:
             self.manifest = manifest_dict
@@ -18,6 +19,9 @@ class DbtDag:
         else:
             raise Exception("No manifest provided.")
 
+    @property
+    def dbt_schema_version(self):
+        return self.manifest["metadata"]["dbt_schema_version"]
+
 
 ddag = DbtDag(manifest_path="examples/jaffle_shop/manifest.json")
-
