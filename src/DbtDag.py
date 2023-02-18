f84cbd78af9cef66993e8fb83181270dbb805804
Vlad Gheorghe <contact@vlad-gheorghe.com>
2023-02-18 10:42:26 +0100

    Create DbtNode object

--- src/DbtDag.py
+++ src/DbtDag.py
@@ -3,6 +3,24 @@ import json
 import networkx as nx
 
 
+class DbtNode:
+    def __init__(self,
+                 full_name: str,
+                 node_dict: dict):
+
+        self.full_name = full_name
+        with full_name.split(".") as parts:
+            self.node_type = parts[0]
+            self.dbt_project = parts[1]
+            self.node_name = parts[2]
+            self.run_id = parts[3] if len(parts) > 3 else None
+
+        self.node_dict = node_dict
+
+    def __hash__(self):
+        return hash(self.full_name)
+
+
 class DbtDag:
     def __init__(self,
                  manifest_path: str = None,
