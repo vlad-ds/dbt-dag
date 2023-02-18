cc4e46f28e28cd0f926536ae104723309ac45646
Vlad Gheorghe <contact@vlad-gheorghe.com>
2023-02-18 10:08:31 +0100

    Initiate DbtDag

--- src/DbtDag.py
+++ src/DbtDag.py
@@ -1,3 +1,23 @@
 import json
 
 import networkx as nx
+
+
+class DbtDag:
+    def __init__(self,
+                manifest_path: str = None,
+                manifest_dict: dict = None):
+
+        self.manifest = None
+
+        if manifest_dict:
+            self.manifest = manifest_dict
+        elif manifest_path:
+            with open(manifest_path) as file:
+                self.manifest = json.load(file)
+        else:
+            raise Exception("No manifest provided.")
+
+
+ddag = DbtDag(manifest_path="examples/jaffle_shop/manifest.json")
+
